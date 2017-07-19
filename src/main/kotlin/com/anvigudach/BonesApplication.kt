ee75805f826f877b921756dc84beebc64b066867
Daniel Garzon <daniel.garzon.sanchez@gmail.com>
2017-07-19 14:05:59 +0200

    Create project using Spring Initializr

---
+++ src/main/kotlin/com/anvigudach/BonesApplication.kt
@@ -0,0 +1,11 @@
+package com.anvigudach
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BonesApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(BonesApplication::class.java, *args)
+}
